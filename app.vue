8280c3e9f59a4b782a20dbf70d1ea74a370b39ab
Rey Garcia <jreygarcia24@gmail.com>
2023-04-12 19:23:15 +0800

    QR Code component challenge.

---
+++ app.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const title = ref('QR Code Component')
+const description = ref('Front-end coding challenge')
+
+useHead({
+  title,
+  meta: [{
+    name: 'description',
+    content: description
+  }]
+})
+</script>
+
+<template>
+  <div class="main">
+    <QRCode />
+  </div>
+</template>
---
+++ components/QRCode.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="QRCode">
+    <div>
+      <img class="QRCode-image" src="@/assets/images/image-qr-code.png" alt="qr-code">
+    </div>
+    <div class="QRCode-overview">
+      <h1>Improve your front-end <br /> skills by building projects</h1>
+    </div>
+    <div class="QRCode-description">
+      <p>Scan the QR code to visit the Frontend Mentor and take your coding skills to the next level</p>
+    </div>
+  </div>
+
+  <footer>
+    <div>
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>
+    </div>
+    <div>
+      Coded by <a href="https://github.com/brandeth" target="_blank">Rey Garcia</a>
+    </div>
+  </footer>
+</template>
